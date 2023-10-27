067dec6ffce72d21498a3430f8e04aee725443ea
QuanKhanz <iamhoangkhang@icloud.com>
2023-10-27 13:40:00 +0700

    [UPDATE] Cập nhật CashCount_DAO và Customer_DAO (Hiện thực interface)

--- src/dao/CashCount_DAO.java
+++ src/dao/CashCount_DAO.java
@@ -13,9 +13,53 @@ import java.sql.*;
  *
  * @author Hoàng Khang
  */
-public class CashCount_DAO {
-    public ArrayList<CashCount> getCashCountsBySheetID(String cashCountSheetID) {
-        ArrayList<CashCount> result = new ArrayList<>();
+public class CashCount_DAO implements interfaces.DAOBase<CashCount> {
+
+    public CashCount_DAO() {
+    }
+
+    public CashCount getCashCount(double value, String cashCountSheetID) {
+        CashCount cashCount = null;
+
+        try {
+            String sql = "SELECT * FROM CashCount WHERE cashCountSheetID = ? AND value = ?";
+            PreparedStatement preparedStatement = ConnectDB.conn.prepareStatement(sql);
+            preparedStatement.setString(1, cashCountSheetID);
+            preparedStatement.setDouble(2, value);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                int quantity = resultSet.getInt("quantity");
+                double total = resultSet.getDouble("total");
+
+                cashCount = new CashCount(quantity, value);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return cashCount;
+    }
+
+    @Override
+    public CashCount getOne(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<CashCount> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    /**
+     * Phương thức lấy toàn các CashCount nằm trong CashCountSheet
+     * @param cashCountSheetID mã phiếu kết toán cần lấy danh sách kiểm tiền
+     * @return ArrayList<CashCount>
+     */
+    public ArrayList<CashCount> getAll(String cashCountSheetID) {
+        ArrayList<CashCount> cashCounts = new ArrayList<>();
+
         try {
             String sql = "SELECT * FROM CashCount WHERE cashCountSheetID = ?";
             PreparedStatement preparedStatement = ConnectDB.conn.prepareStatement(sql);
@@ -24,35 +68,58 @@ public class CashCount_DAO {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                int quantity = resultSet.getInt("quantity");
                 double value = resultSet.getDouble("value");
+                int quantity = resultSet.getInt("quantity");
+                double total = resultSet.getDouble("total");
 
-                // Thêm đối tượng CashCount vào danh sách kết quả
-                result.add(new CashCount(quantity, value));
+                CashCount cashCount = new CashCount(quantity, value);
+                cashCounts.add(cashCount);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return result;
+
+        return cashCounts;
+    }
+
+    @Override
+    public String generateID() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Boolean create(CashCount cashCount) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
-     public boolean saveCashCounts(String cashCountSheetID, ArrayList<CashCount> cashCounts) {
-        try {
-            // Tiến hành thêm mới dữ liệu
-            String sql = "INSERT INTO CashCount (cashCountSheetID, quantity, value) VALUES (?, ?, ?)";
-            PreparedStatement preparedStatement = ConnectDB.conn.prepareStatement(sql);
-            for (CashCount cashCount : cashCounts) {
-                preparedStatement.setString(1, cashCountSheetID);
-                preparedStatement.setInt(2, cashCount.getQuantity());
-                preparedStatement.setDouble(3, cashCount.getValue());
+    public Boolean create(CashCount cashCount, String cashCountSheetID) {
+    try {
+        String sql = "INSERT INTO CashCount (cashCountSheetID, value, quantity, total) VALUES (?, ?, ?, ?)";
+        PreparedStatement preparedStatement = ConnectDB.conn.prepareStatement(sql);
 
-                preparedStatement.executeUpdate();
-            }
+        preparedStatement.setString(1, cashCountSheetID);
+        preparedStatement.setDouble(2, cashCount.getValue());
+        preparedStatement.setInt(3, cashCount.getQuantity());
+        preparedStatement.setDouble(4, cashCount.getTotal());
 
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+        int rowsAffected = preparedStatement.executeUpdate();
+        if (rowsAffected > 0) {
+            return true; // Thành công
         }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return false; // Thất bại
+}
+
+
+    @Override
+    public Boolean update(String id, CashCount newObject) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Boolean delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
