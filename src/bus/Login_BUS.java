2abd789d987ae2f407077d38b78ee119508bc274
QuanKhanz <iamhoangkhang@icloud.com>
2023-11-14 11:35:19 +0700

    Merge branch 'main' of https://github.com/thanhcanhit/omega-book-2

--- src/bus/Login_BUS.java
+++ src/bus/Login_BUS.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -45,4 +46,6 @@ public class Login_BUS {
         }
         return accountDAO.updatePass(acc.getEmployee().getEmployeeID(), passNewHash);
     }
+
+    
 }
