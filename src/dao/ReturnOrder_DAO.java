2d16878efe1a3a2d5325cdfaca1abbbcdedf76ab
nhutam03 <nhutam050@gmail.com>
2023-12-09 14:23:56 +0700

    [UPDATE] Sửa những chỗ warning

--- src/dao/ReturnOrder_DAO.java
+++ src/dao/ReturnOrder_DAO.java
@@ -13,8 +13,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
