15d98f6a178c2f7834fe0b9ac0a8d51f15e114f0
nhutam03 <nhutam050@gmail.com>
2023-12-10 16:23:52 +0700

    [UPDATE] Chỉnh sửa menu cho nhân viên

--- src/gui/menu/Menu.java
+++ src/gui/menu/Menu.java
@@ -43,7 +43,8 @@ public class Menu extends JPanel {
 //    row col
     private static final int employeeItemsBan[][] = {
         {1, 1},
-        {2, 0}, {2, 1}, {2, 2},
+        //{2, 0}, {2, 1}, {2, 2},
+        {2, 2},
         {3, 0}, {3, 1}, {3, 2},
         {5, 0},
         {9, 1}, {9, 2}
